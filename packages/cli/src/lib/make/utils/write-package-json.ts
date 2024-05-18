29cb3227bc3d22dd69b9ff52984d0d101d0c45f8
JoeyDoey <joe@nyaggah.com>
2024-05-18 12:29:43 -0700

    chore: ensure bedframe/cli and /core are right versions to write to package.json

--- packages/cli/src/lib/make/utils/write-package-json.ts
+++ packages/cli/src/lib/make/utils/write-package-json.ts
@@ -100,8 +100,8 @@ export function writePackageJson(response: prompts.Answers<string>): void {
     }
   },
   "devDependencies": {
-    "@bedframe/cli": "0.0.78",
-    "@bedframe/core": "0.0.42",
+    "@bedframe/cli": "0.0.79",
+    "@bedframe/core": "0.0.43",
 ${
   changesets
     ? `"@changesets/cli": "^2.27.1",
