075697845f3b600f17742e23ea978192e184c02b
JoeyDoey <joe@nyaggah.com>
2025-06-22 15:44:47 -0700

    chore(publish): code clean up

--- packages/cli/src/commands/publish.ts
+++ packages/cli/src/commands/publish.ts
@@ -259,13 +259,6 @@ export const publishCommand = new Command('publish')
     'specify browsers to publish (chrome, firefox, edge)',
   )
   .action(async (options) => {
-    // TO diddly DO:
-    // actually handle multiple browsers --browsers
-    // option... run through below conditional for all browsers
-    // found in ./src/manifests/index.ts array --> []
-    // ^^^ update this when there's a moar betta way to grab all
-    // browsers from project. e.g. esm import that ts and process
-    // the func to return the pojo bedframeConfig
     try {
       const selectedBrowsers = options.browsers
       if (
